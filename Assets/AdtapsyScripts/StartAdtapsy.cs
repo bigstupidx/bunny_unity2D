6cac52ae3e5e88e02c2fb354218496a960e3566e
ridhwandaud <muhammadridhwandaud@gmail.com>
2016-06-18 12:41:08 +0800

    add appodeal , still bug

--- Assets/AdtapsyScripts/StartAdtapsy.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class StartAdtapsy : MonoBehaviour {
-	
-	void Awake () {
-		AdTapsy.StartSessionAndroid("57625303e4b03255c3574811");   
-	}
-}
